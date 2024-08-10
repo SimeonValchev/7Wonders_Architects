b82dab22ee31a3453dc2db542540502aa556b8fd
SimeonValchev <simeon.v.valchev@gmail.com>
2024-08-10 15:23:56 +0200

    Layout finished

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ deck_left = [0] + [10] * 14
 deck_right = [0] + [10] * 14
 
 my_decks = [deck_cent, deck_left, deck_right]
-offset = {'alexandria': -129, 'artemis': 13, 'babylon': 79, 'gizeh': 76, 'hallicarnas': -162, 'rhodes': -17, 'zeus': -91}
+inventions = [1] * 15
 
 
 class Player:
@@ -48,8 +48,11 @@ class MyFrame(wx.Frame):
 
     def OnPaint(self, event):
 
-        wonder_leftside = wx.Bitmap('Assets/wonder_babylon.png', wx.BITMAP_TYPE_PNG).GetSubBitmap((0, 0, 467, 421))
-        wonder_rightside = wx.Bitmap('Assets/wonder_alexandria.png', wx.BITMAP_TYPE_PNG).GetSubBitmap((0, 0, 467, 629))
+        wonder_leftside = wx.Bitmap('Assets/wonder_babylon.png', wx.BITMAP_TYPE_PNG).GetSubBitmap((0, 0, 350, 318))
+        wonder_rightside = wx.Bitmap('Assets/wonder_alexandria.png', wx.BITMAP_TYPE_PNG).GetSubBitmap((0, 0, 350, 472))
+        icons = wx.Bitmap('Assets/main_icons.png', wx.BITMAP_TYPE_PNG)
+        peace_icons = wx.Bitmap('Assets/war_icons.png', wx.BITMAP_TYPE_PNG).GetSubBitmap((75, 0, 75, 75))
+        invent_icons = wx.Bitmap('Assets/progress - Copy.png', wx.BITMAP_TYPE_PNG).GetSubBitmap((0, 0, 100, 100))
 
         # BACKGROUND
         bg_image = wx.Bitmap('Assets/background.png', wx.BITMAP_TYPE_PNG)
@@ -64,10 +67,18 @@ class MyFrame(wx.Frame):
             for y in range(0, panel_size.height, img_height):
                 dc.DrawBitmap(bg_image, x, y)
 
-        # WONDERS
+        # TRANSPARENT PNGs
         dc.SetBackground(wx.Brush("WHITE"))
-        dc.DrawBitmap(wonder_leftside, 50, self.GetSize()[1]-200 - wonder_leftside.GetSize()[1], True)
-        dc.DrawBitmap(wonder_rightside, self.GetSize()[0]-517, self.GetSize()[1]-200 - wonder_rightside.GetSize()[1], True)
+        dc.DrawBitmap(wonder_leftside, 100, self.GetSize()[1]-200 - wonder_leftside.GetSize()[1], True)
+        dc.DrawBitmap(icons, 50, self.GetSize()[1]-200, True)
+        dc.DrawBitmap(peace_icons, 50+93, 63, True)
+        dc.DrawBitmap(peace_icons, 135+93, 63, True)
+        dc.DrawBitmap(peace_icons, 220+93, 63, True)
+        dc.DrawBitmap(invent_icons, self.GetSize()[0] - 480, 50, True)
+        dc.DrawBitmap(invent_icons, self.GetSize()[0] - 370, 50, True)
+        dc.DrawBitmap(invent_icons, self.GetSize()[0] - 260, 50, True)
+        dc.DrawBitmap(invent_icons, self.GetSize()[0] - 150, 50, True)
+        dc.DrawBitmap(wonder_rightside, self.GetSize()[0]-450, self.GetSize()[1]-200 - wonder_rightside.GetSize()[1], True)
 
     def __init__(self, parent, title, decks):
         super(MyFrame, self).__init__(parent, title=title, style=wx.MAXIMIZE)
@@ -95,18 +106,19 @@ class MyFrame(wx.Frame):
         card_left_image = self.cards.GetSubBitmap((100 * self.card_left, 0, 100, 148))
         card_right_image = self.cards.GetSubBitmap((100 * self.card_right, 0, 100, 148))
 
+        # INIT IMAGE for INVENTIONS
+
         self.panel = MyPanel(self)
+        #INVENTIONS CLICK
+        #self.panel.Bind(wx.EVT_LEFT_DOWN, lambda event: self.draw_card('left'))
 
         # BACKGROUND
         self.panel.Bind(wx.EVT_PAINT, self.OnPaint)
 
         # QUIT BUTTON
-        quit_image = wx.Bitmap('Assets/quit.png', wx.BITMAP_TYPE_PNG)
-        quit_image_hover = wx.Bitmap('Assets/quit_hover.png', wx.BITMAP_TYPE_PNG)
-        button_quit = wx.BitmapButton(self.panel, bitmap=quit_image, style=wx.NO_BORDER)
-
-        button_quit.SetPosition(
-            (frame_size[0] - button_quit.GetSize()[0] - 10, frame_size[1] - button_quit.GetSize()[1] - 10))
+        quit_image = wx.Bitmap('Assets/ex.png', wx.BITMAP_TYPE_PNG)
+        quit_image_hover = wx.Bitmap('Assets/exq.png', wx.BITMAP_TYPE_PNG)
+        button_quit = wx.BitmapButton(self.panel, bitmap=quit_image, style=wx.NO_BORDER, pos=(frame_size[0] - 30, 0))
 
         button_quit.Bind(wx.EVT_BUTTON, lambda event: self.Close())
         button_quit.Bind(wx.EVT_ENTER_WINDOW, lambda event: button_quit.SetBitmap(quit_image_hover))
@@ -116,9 +128,9 @@ class MyFrame(wx.Frame):
         self.button_cent = wx.BitmapButton(self.panel, bitmap=card_cent_image, style=wx.NO_BORDER,
                                            pos=((frame_size[0] // 2) - 50, 50))
         self.button_left = wx.BitmapButton(self.panel, bitmap=card_left_image, style=wx.NO_BORDER,
-                                           pos=(100, (frame_size[1] // 2) - 74))
+                                           pos=((frame_size[0] // 2)-200, (frame_size[1] // 2)-25))
         self.button_right = wx.BitmapButton(self.panel, bitmap=card_right_image, style=wx.NO_BORDER,
-                                            pos=(frame_size[0] - 200, (frame_size[1] // 2) - 74))
+                                            pos=((frame_size[0] // 2)+100, (frame_size[1] // 2)-25))
 
         # BIND DECK-BUTTONS to FUNCTIONALITY
         self.button_cent.Bind(wx.EVT_BUTTON, lambda event: self.draw_card('cent'))
